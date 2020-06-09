0d8dc346a156635995c2286cf2b0d5b6df15165d
quidvil <gurprit.iiest@gmail.com>
2020-06-09 21:57:52 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.inputContainer} placeholder='Your Todos'/>
+                <Button title='ADD'/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    input: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        width: '80%'
+    }
 });
